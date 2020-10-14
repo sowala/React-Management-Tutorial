a8f866310106b9e0e4365b1dac6a75324c9e11fd
huni <winers79@gmail.com>
2020-10-14 13:02:48 +0900

    Create React Project

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+## Management System
+강의 
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customers=[
+  {
+    id: 1,
+    image: 'https://placeimg.com/64/64/1',
+    name: '도라에몽',
+    birthday: '19/12/14',
+    gender: "남자",
+    job: '대학생'
+  },
+  {
+    id: 2,
+    image: 'https://placeimg.com/64/64/2',
+    name: '홍길동',
+    birthday: '19/12/14',
+    gender: "남자",
+    job: '대학생'
+  },
+  {
+    id: 3,
+    image: 'https://placeimg.com/64/64/3',
+    name: '피카츄',
+    birthday: '19/12/14',
+    gender: "몬스터",
+    job: '대학생'
+  }
+]
+
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        {
+          customers.map(c =>{
+            console.log(c)
+            return(
+              <Customer
+                key={c.id} // map 함수를 쓸때는 key 속성 값을 넣어주어야 한다.
+                image={c.image}
+                id={c.id}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+        
+      </div>
+      
+    )
+  }
 }
 
 export default App;
