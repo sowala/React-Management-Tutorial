2d661805db3dc18df4c08c9e80845eb36610798d
huni <winers79@gmail.com>
2020-10-15 13:26:23 +0900

    Add nodejs express sever module

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res)=>{
+    res.send('ok');
+})
+
+app.listen(port, ()=> console.log(`connet - ${port}`));
