d8c48a8d642b886107f9e772982d0f609b8f3425
huni <winers79@gmail.com>
2020-10-16 13:07:34 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import {withStyles} from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   root: {
@@ -17,38 +18,65 @@ const styles = theme => ({
   },
   table:{
     minWidth: 1080
+  },
+  progress:{
+    margin: theme.spacing.unit *2
   }
 })
 
-const customers=[
-  {
-    id: 1,
-    image: 'https://placeimg.com/64/64/1',
-    name: '도라에몽',
-    birthday: '19/12/14',
-    gender: "남자",
-    job: '대학생'
-  },
-  {
-    id: 2,
-    image: 'https://placeimg.com/64/64/2',
-    name: '홍길동',
-    birthday: '19/12/14',
-    gender: "남자",
-    job: '대학생'
-  },
-  {
-    id: 3,
-    image: 'https://placeimg.com/64/64/3',
-    name: '피카츄',
-    birthday: '19/12/14',
-    gender: "몬스터",
-    job: '대학생'
-  }
-]
+// const customers=[
+//   {
+//     id: 1,
+//     image: 'https://placeimg.com/64/64/1',
+//     name: '도라에몽',
+//     birthday: '19/12/14',
+//     gender: "남자",
+//     job: '대학생'
+//   },
+//   {
+//     id: 2,
+//     image: 'https://placeimg.com/64/64/2',
+//     name: '홍길동',
+//     birthday: '19/12/14',
+//     gender: "남자",
+//     job: '대학생'
+//   },
+//   {
+//     id: 3,
+//     image: 'https://placeimg.com/64/64/3',
+//     name: '피카츄',
+//     birthday: '19/12/14',
+//     gender: "몬스터",
+//     job: '대학생'
+//   }
+// ]
 
 
 class App extends Component{
+  state={
+    customers: "",
+    completed: 0
+  }
+
+  
+  componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
+    this.callApi().then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async() =>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () =>{
+    const {completed} = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed + 1});
+  }
+
+
   render(){
     const {classes} = this.props;
     return(
@@ -65,20 +93,24 @@ class App extends Component{
             </TableRow>
           </TableHead>
           <TableBody>
-          {
-              customers.map(c =>{
-                return(
-                  <Customer
-                    key={c.id} // map 함수를 쓸때는 key 속성 값을 넣어주어야 한다.
-                    image={c.image}
-                    id={c.id}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}/>
-                  )
-                })
-            } 
+          {this.state.customers ? this.state.customers.map(c =>{
+              return(
+                <Customer
+                  key={c.id} // map 함수를 쓸때는 key 속성 값을 넣어주어야 한다.
+                  image={c.image}
+                  id={c.id}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}/>)
+                }) : 
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                  </TableCell>
+                </TableRow>
+                } 
+            
           </TableBody>
         </Table>
       </Paper>          
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res)=>{
-    res.send('ok');
+app.get('/api/customers', (req, res)=>{
+    res.send([
+            {
+              id: 1,
+              image: 'https://placeimg.com/64/64/1',
+              name: '도라에몽',
+              birthday: '19/12/14',
+              gender: "남자",
+              job: '대학생'
+            },
+            {
+              id: 2,
+              image: 'https://placeimg.com/64/64/2',
+              name: '홍길동',
+              birthday: '19/12/14',
+              gender: "남자",
+              job: '대학생'
+            },
+            {
+              id: 3,
+              image: 'https://placeimg.com/64/64/3',
+              name: '피카츄',
+              birthday: '19/12/14',
+              gender: "몬스터",
+              job: '대학생'
+            }
+    ]);
 })
 
 app.listen(port, ()=> console.log(`connet - ${port}`));
