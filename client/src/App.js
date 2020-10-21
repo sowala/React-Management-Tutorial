ba776c0267030b952a5b5fbed1fbf65b396a3ff7
huni <winers79@gmail.com>
2020-10-21 11:26:28 +0900

    add customer delete module

--- client/src/App.js
+++ client/src/App.js
@@ -109,12 +109,14 @@ class App extends Component{
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
             {this.state.customers ? this.state.customers.map(c =>{
                 return(
                   <Customer
+                    stateRefresh={this.stateRefresh}
                     key={c.id} // map 함수를 쓸때는 key 속성 값을 넣어주어야 한다.
                     image={c.image}
                     id={c.id}
--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const multer = require('multer');
 const upload = multer({dest: './upload'})
 
 app.get('/api/customers', (req, res)=>{
-  connection.query("SELECT * FROM customer", (err, rows)=>{
+  connection.query("SELECT * FROM customer WHERE isDeleted = 0", (err, rows)=>{
     res.send(rows);
   })
 })
@@ -36,7 +36,7 @@ app.use('/image', express.static('./upload'));
 app.post('/api/customers', upload.single('image'), (req, res)=>{
   //upload.single('image') >> FormData 에 변수값을 따라간다.
   // console.log(req.body)
-  let sql = 'INSERT INTO customer VALUES(null, ?, ?, ?, ? ,?)';
+  let sql = 'INSERT INTO customer VALUES(null, ?, ?, ?, ? ,?, now(), 0)';
   let image = 'http://localhost:5000/image/' + req.file.filename;
   let name = req.body.name;
   let birthday = req.body.birthday;
@@ -44,6 +44,14 @@ app.post('/api/customers', upload.single('image'), (req, res)=>{
   let gender = req.body.gender;
   let params = [image, name, birthday, gender, job];
   connection.query(sql, params,(err, rows)=>{
+    res.send(rows);
+  })
+})
+
+app.delete('/api/customers/:id', (req, res)=>{
+  let sql = 'UPDATE customer SET isDeleted =1 WHERE id = ?';
+  let params = [req.params.id];
+  connection.query(sql, params, (err, rows)=>{
     res.send(rows);
   })
 })
